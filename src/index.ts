4f8328e11baa8aaf45e82b9375784ee59d8e873d
keleris32 <charles.ejiegbu@gmail.com>
2022-02-07 14:38:04 +0100

    feat: Setup express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
 require('dotenv').config();
+
+import express, { Request, Response, Application } from 'express';
+import cors from 'cors';
+
+const app: Application = express();
+
+app.use(cors()); // Cross-Origin-Resource-Sharing
+app.use(express.json()); // Parse to JSON
+
+// Routes
+app.get(
+  '/',
+  (_req: Request, res: Response): Response => res.send('Hello from Homepage')
+);
+
+app.listen(process.env.PORT || 4000, (): void =>
+  console.log(`Conected to port!`)
+);
