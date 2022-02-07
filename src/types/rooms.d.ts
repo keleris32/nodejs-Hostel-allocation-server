3dffa7e77c157bbfc6eec6c97ec6cb50e955c52b
keleris32 <charles.ejiegbu@gmail.com>
2022-02-07 14:37:33 +0100

    feat: added types for students and rooms models/entities

---
+++ src/types/rooms.d.ts
@@ -0,0 +1,10 @@
+export interface Rooms {
+  id: number;
+  hostel: string;
+  room_type: string;
+  room_number: string;
+  price: number;
+  no_of_habitants: number;
+  type_of_hostel: number;
+  created_at?: Date;
+}
---
+++ src/types/students.d.ts
@@ -0,0 +1,12 @@
+export interface Students {
+  id: number;
+  room_id?: number;
+  name: string;
+  level: string;
+  course: string;
+  matric_no: string;
+  password: string;
+  gender: string;
+  created_at?: Date;
+  updated_at?: Date;
+}
