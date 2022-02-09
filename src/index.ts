132dba065ee95cc800ef9522fde9afd4b4cd8cff
keleris32 <charles.ejiegbu@gmail.com>
2022-02-09 23:08:09 +0100

    added new routes

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,11 @@ import cookieParser from 'cookie-parser';
 // Routes
 import authRouter from './routes/authRoutes';
 import refreshTokenRouter from './routes/refreshTokenRoute';
+import roomsRouter from './routes/roomsRoute';
+import paymentIntentRouter from './routes/createPaymentIntentRoute';
+import webhookRouter from './routes/paystackWebhookRoute';
+import studentRouter from './routes/studentsRoute';
+import createTablesRouter from './routes/createDbTables';
 
 const app: Application = express();
 
@@ -19,6 +24,11 @@ app.use(cookieParser()); // Parse cookies from req
 // API Routes
 app.use('/api/auth', authRouter); // Auth route
 app.use('/api', refreshTokenRouter); // refresh-token route
+app.use('/api', roomsRouter); // Rooms route
+app.use('/api', paymentIntentRouter); // Paystack payment intent route
+app.use('/api', webhookRouter); // Webhook route to verify payments and allocate room if successful
+app.use('/api', studentRouter); // Students route
+app.use('/api', createTablesRouter); // Create DB tables
 
 app.listen(process.env.PORT || 4000, (): void =>
   console.log(`Conected to port!`)
