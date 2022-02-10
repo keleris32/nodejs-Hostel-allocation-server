4aba8ee8edf84e4ecb6660e6a14c022096a63fab
keleris32 <charles.ejiegbu@gmail.com>
2022-02-10 16:59:51 +0100

    Fix: changed typo and refactored handling success and error response

--- dist/middlewares/verifyPaystackPayment.js
+++ dist/middlewares/verifyPaystackPayment.js
@@ -11,7 +11,7 @@ const errorResponse_1 = require('../utils/errorResponse');
 const successResponse_1 = require('../utils/successResponse');
 const dbConnector_1 = __importDefault(require('../config/dbConnector'));
 
-const allocateRoomController = async (metadad) => {
+const allocateRoomController = async (metadata, res) => {
   try {
     const room_id = metadata.user.room_id;
     const matricNumber = metadata.user.matricNumber;
@@ -19,24 +19,23 @@ const allocateRoomController = async (metadad) => {
       'UPDATE students SET room_id = $1 WHERE matric_no = $2',
       [room_id, matricNumber]
     );
-    const responseMessage = 'Room allocated successfully!';
-    const responseData = {};
-    res
-      .status(201)
-      .json(
-        (0, successResponse_1.successResponseBody)(
-          responseMessage,
-          responseData
-        )
-      );
+    // const responseMessage = 'Room allocated successfully!';
+    // const responseData = {};
+    // res
+    //   .status(201)
+    //   .json(
+    //     (0, successResponse_1.successResponseBody)(
+    //       responseMessage,
+    //       responseData
+    //     )
+    //   );
+    res.sendStatus(200);
   } catch (error) {
-    res
-      .status(500)
-      .json((0, errorResponse_1.errorResponseBody)('Internal Server Error!'));
+    console.log(error);
   }
 };
 
-const verifyPayment = (req, res) => {
+const verifyPayment = async (req, res) => {
   try {
     const secret = process.env.PAYSTACK_SECRET_KEY;
     const hash = crypto_1.default
@@ -47,7 +46,7 @@ const verifyPayment = (req, res) => {
       const event = req.body;
       if (event.data.status === 'success') {
         // Call fn to allocate room on success
-        allocateRoomController(event.data.metadata);
+        allocateRoomController(event.data.metadata, res);
       } else {
         res.sendStatus(500);
       }
