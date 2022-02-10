418a18bdfa661f8cf2c29fca72bf92d12bfeac09
keleris32 <charles.ejiegbu@gmail.com>
2022-02-10 21:51:42 +0100

    feat: Updated cors url

--- dist/index.js
+++ dist/index.js
@@ -1,21 +1,34 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
 require('dotenv').config();
-const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
-const cookie_parser_1 = __importDefault(require("cookie-parser"));
-const authRoutes_1 = __importDefault(require("./routes/authRoutes"));
-const refreshTokenRoute_1 = __importDefault(require("./routes/refreshTokenRoute"));
-const roomsRoute_1 = __importDefault(require("./routes/roomsRoute"));
-const createPaymentIntentRoute_1 = __importDefault(require("./routes/createPaymentIntentRoute"));
-const paystackWebhookRoute_1 = __importDefault(require("./routes/paystackWebhookRoute"));
-const studentsRoute_1 = __importDefault(require("./routes/studentsRoute"));
-const createDbTables_1 = __importDefault(require("./routes/createDbTables"));
+const express_1 = __importDefault(require('express'));
+const cors_1 = __importDefault(require('cors'));
+const cookie_parser_1 = __importDefault(require('cookie-parser'));
+const authRoutes_1 = __importDefault(require('./routes/authRoutes'));
+const refreshTokenRoute_1 = __importDefault(
+  require('./routes/refreshTokenRoute')
+);
+const roomsRoute_1 = __importDefault(require('./routes/roomsRoute'));
+const createPaymentIntentRoute_1 = __importDefault(
+  require('./routes/createPaymentIntentRoute')
+);
+const paystackWebhookRoute_1 = __importDefault(
+  require('./routes/paystackWebhookRoute')
+);
+const studentsRoute_1 = __importDefault(require('./routes/studentsRoute'));
+const createDbTables_1 = __importDefault(require('./routes/createDbTables'));
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)());
+app.use(
+  (0, cors_1.default)({
+    origin: process.env.CORS_ORIGIN_URL,
+    credentials: true,
+  })
+);
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 app.use('/api/auth', authRoutes_1.default);
