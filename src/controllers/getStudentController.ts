a49ec851711539114b2e4d7c35f584d3664a9ddd
keleris32 <charles.ejiegbu@gmail.com>
2022-02-13 15:37:41 +0100

    re-implement student logic for tsc file

--- src/controllers/getStudentController.ts
+++ src/controllers/getStudentController.ts
@@ -10,15 +10,27 @@ export const getStudent = async (req: Request, res: Response) => {
     const matricNumber = req.user!.matricNumber;
 
     const student = await pool.query(
-      'SELECT students.*, rooms.hostel, rooms.room_type, rooms.room_number, rooms.price, rooms.no_of_inhabitants, rooms.type_of_hostel FROM students INNER JOIN rooms ON students.room_id = rooms.id WHERE matric_no = $1',
+      'SELECT * FROM students WHERE matric_no = $1',
       [matricNumber]
     );
 
+    let responseMessage = 'Student data fetched successfully!';
+
     if (student.rowCount !== 0) {
-      let responseMessage = 'Student data fetched successfully!';
-      res
-        .status(200)
-        .json(successResponseBody(responseMessage, student.rows[0]));
+      if (student.rows[0].room_id !== null) {
+        const studentWithRoom = await pool.query(
+          'SELECT students.*, rooms.hostel, rooms.room_type, rooms.room_number, rooms.price, rooms.no_of_inhabitants, rooms.type_of_hostel FROM students INNER JOIN rooms ON students.room_id = rooms.id WHERE matric_no = $1',
+          [matricNumber]
+        );
+
+        res
+          .status(200)
+          .json(successResponseBody(responseMessage, studentWithRoom.rows[0]));
+      } else {
+        res
+          .status(200)
+          .json(successResponseBody(responseMessage, student.rows[0]));
+      }
     } else {
       throw new Error('Could not fetch student data!');
     }
