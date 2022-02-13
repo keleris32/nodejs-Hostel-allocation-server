af78e7c3fd397c224f4724fbe3238409ba15d752
keleris32 <charles.ejiegbu@gmail.com>
2022-02-13 12:22:13 +0100

    backtrack ts

--- src/controllers/getStudentController.ts
+++ src/controllers/getStudentController.ts
@@ -7,29 +7,18 @@ import pool from '../config/dbConnector';
 
 export const getStudent = async (req: Request, res: Response) => {
   try {
-    const { id } = req.body;
+    const matricNumber = req.user!.matricNumber;
 
-    const student = await pool.query('SELECT * FROM students WHERE id = $1', [
-      id,
-    ]);
-
-    const responseMessage = 'Student data fetched successfully!';
+    const student = await pool.query(
+      'SELECT students.*, rooms.hostel, rooms.room_type, rooms.room_number, rooms.price, rooms.no_of_inhabitants, rooms.type_of_hostel FROM students INNER JOIN rooms ON students.room_id = rooms.id WHERE matric_no = $1',
+      [matricNumber]
+    );
 
     if (student.rowCount !== 0) {
-      if (student.rows[0].room_id !== null) {
-        let studentWithRoom = await pool.query(
-          'SELECT students.*, rooms.hostel, rooms.room_type, rooms.room_number, rooms.price, rooms.no_of_inhabitants, rooms.type_of_hostel FROM students INNER JOIN rooms ON students.room_id = rooms.id WHERE id = $1',
-          [id]
-        );
-
-        res
-          .status(200)
-          .json(successResponseBody(responseMessage, studentWithRoom.rows[0]));
-      } else {
-        res
-          .status(200)
-          .json(successResponseBody(responseMessage, student.rows[0]));
-      }
+      let responseMessage = 'Student data fetched successfully!';
+      res
+        .status(200)
+        .json(successResponseBody(responseMessage, student.rows[0]));
     } else {
       throw new Error('Could not fetch student data!');
     }
