ad87eed18795616294c348cd1dcc48921ee83026
keleris32 <charles.ejiegbu@gmail.com>
2022-02-18 14:31:07 +0100

    refactor: added controller to webhook route

--- dist/routes/paystackWebhookRoute.js
+++ dist/routes/paystackWebhookRoute.js
@@ -7,7 +7,12 @@ var __importDefault =
 Object.defineProperty(exports, '__esModule', { value: true });
 const express_1 = __importDefault(require('express'));
 const verifyPaystackPayment_1 = require('../middlewares/verifyPaystackPayment');
+const allocateRoomController_1 = require('../controllers/rooms/allocateRoomController');
 const webhookRouter = express_1.default.Router();
-webhookRouter.post('/paystack-webhook', verifyPaystackPayment_1.verifyPayment);
+webhookRouter.post(
+  '/paystack-webhook',
+  verifyPaystackPayment_1.verifyPayment,
+  allocateRoomController_1.allocateRoomController
+);
 exports.default = webhookRouter;
 //# sourceMappingURL=paystackWebhookRoute.js.map
