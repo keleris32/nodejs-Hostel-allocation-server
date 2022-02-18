f82cf250e1acc723df6d815690819f1dcb16fdd6
keleris32 <charles.ejiegbu@gmail.com>
2022-02-18 14:30:40 +0100

    refactor: used req.locals to pass metadata obj through middleware

--- dist/middlewares/verifyPaystackPayment.js
+++ dist/middlewares/verifyPaystackPayment.js
@@ -7,37 +7,24 @@ var __importDefault =
 Object.defineProperty(exports, '__esModule', { value: true });
 exports.verifyPayment = void 0;
 const crypto_1 = __importDefault(require('crypto'));
-const redis_1 = __importDefault(require('redis'));
-const promisify_1 = __importDefault(require('util'));
 const errorResponse_1 = require('../utils/errorResponse');
 const successResponse_1 = require('../utils/successResponse');
-const dbConnector_1 = __importDefault(require('../config/dbConnector'));
+// const dbConnector_1 = __importDefault(require('../config/dbConnector'));
 
-const client = redis_1.default.createClient({
-  url: process.env.REDIS_URI,
-  socket: {
-    tls: true,
-    rejectUnauthorized: false,
-  },
-});
+// const allocateRoomController = async (metadata, res) => {
+//   try {
+//     const room_id = metadata.room_id;
+//     const student_id = metadata.student_id;
+//     await dbConnector_1.default.query(
+//       'UPDATE students SET room_id = $1 WHERE id = $2',
+//       [room_id, student_id]
+//     );
+//   } catch (error) {
+//     console.log('Errrrrrrrrr', error.response);
+//   }
+// };
 
-const GET_ASYNC = promisify_1.promisify(client.get).bind(client);
-const SET_ASYNC = promisify_1.promisify(client.set).bind(client);
-
-const allocateRoomController = async (metadata, res) => {
-  try {
-    const room_id = metadata.room_id;
-    const student_id = metadata.student_id;
-    await dbConnector_1.default.query(
-      'UPDATE students SET room_id = $1 WHERE id = $2',
-      [room_id, student_id]
-    );
-  } catch (error) {
-    console.log('Errrrrrrrrr', error.response);
-  }
-};
-
-const verifyPayment = async (req, res) => {
+const verifyPayment = async (req, res, next) => {
   try {
     const secret = process.env.PAYSTACK_SECRET_KEY;
     const hash = crypto_1.default
@@ -47,20 +34,15 @@ const verifyPayment = async (req, res) => {
     if (hash === req.headers['x-paystack-signature']) {
       const event = req.body;
       if (event.data.status === 'success') {
-        const saveResult = await SET_ASYNC(
-          'webhook',
-          JSON.stringify(event.data.metadata),
-          'EX',
-          20
-        );
-
-        console.log('Cached!!!!', saveResult);
+        req.locals.roomId = event.data.metadata.room_id;
+        req.locals.studentId = event.data.metadata.student_id;
 
         // console.log(JSON.stringify(event.data, null, 2));
         res.sendStatus(200);
 
+        return next();
         // Call fn to allocate room on success
-        await allocateRoomController(event.data.metadata, res);
+        // await allocateRoomController(event.data.metadata, res);
       } else {
         res.sendStatus(500);
       }
